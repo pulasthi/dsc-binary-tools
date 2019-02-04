eae848a22d47455e9f724e636ebbf99b2bfff4cb
pulasthi <pulasthi911@gmail.com>
2019-02-04 11:32:29 -0500

    adding test class

--- src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/MPITest.java
+++ src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/MPITest.java
@@ -8,10 +8,22 @@ public class MPITest {
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
 
-        int myrank = MPI.COMM_WORLD.getRank();
-        int size = MPI.COMM_WORLD.getSize() ;
-        System.out.println("Hello world from rank " + myrank + " of " + size);
+        int rank = MPI.COMM_WORLD.getRank(),
+                size = MPI.COMM_WORLD.getSize(),
+                nint = 100; // Intervals.
+        double h = 1.0/(double)nint, sum = 0.0;
 
+        for(int i=rank+1; i<=nint; i+=size) {
+            double x = h * ((double)i - 0.5);
+            sum += (4.0 / (1.0 + x * x));
+        }
+
+        double sBuf[] = { h * sum },
+                rBuf[] = new double[1];
+
+        MPI.COMM_WORLD.reduce(sBuf, rBuf, 1, MPI.DOUBLE, MPI.SUM, 0);
+
+        if(rank == 0) System.out.println("PI: " + rBuf[0]);
         MPI.Finalize();
     }
 }
