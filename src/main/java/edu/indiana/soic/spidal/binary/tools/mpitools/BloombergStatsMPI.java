5e4ed0730a9c0418cc33c4582fe8d0e01042cf87
pulasthi <pulasthi911@gmail.com>
2019-02-05 00:40:21 -0500

    adding sd calculation

--- src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergStatsMPI.java
+++ src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergStatsMPI.java
@@ -20,10 +20,14 @@ public class BloombergStatsMPI {
 
             double max = Double.MIN_VALUE;
             double min = Double.MAX_VALUE;
+            double sum = 0.0;
             double count = 0;
             double missingCount = 0;
-            int currentMax = 0;
+            int currentMax = -1;
             int countWrong = 0;
+            int countOver1k = 0;
+
+            double sdsum = 0.0;
 
             for (int i = 0; i < filesPerProc; i++) {
                 int fileIndex = ParallelOps.worldProcRank * filesPerProc + i;
@@ -35,29 +39,25 @@ public class BloombergStatsMPI {
                 String splits[];
                 while ((line = bf.readLine()) != null) {
                     splits = line.split("\\s+");
-                    if (splits.length < 3) {
-                        countWrong++;
-                        continue;
-                    }
-                    if (splits[0].equals("") || splits[1].equals("") || splits[2].equals("")) {
-                        countWrong++;
-                        continue;
-                    }
+
                     int row = Integer.valueOf(splits[0]);
                     int col = Integer.valueOf(splits[1]);
                     double value = Double.valueOf(splits[2]);
+                    sum += value;
                     max = (value > max) ? value : max;
                     min = (min > value) ? value : min;
+
+                    if(value > 1000) countOver1k++;
 
                     count++;
                     if(ParallelOps.worldProcRank == 0 && (count % 20000000 == 0)){
                         System.out.print(".");
                     }
                     if (currentMax > row) {
-                        throw new IllegalStateException("File not in order" + count);
+                        throw new IllegalStateException("File : " + fileId + " not in order at line : " + count);
                     }
 
-                    if (row > currentMax) {
+                    if (currentMax > 0 && row > currentMax) {
                         missingCount += (row - currentMax - 1);
                     }
                     currentMax = row;
@@ -65,17 +65,39 @@ public class BloombergStatsMPI {
 
 
             }
-
+            System.out.println();
             max = ParallelOps.allReduceMax(max);
             min = ParallelOps.allReduceMin(min);
             count = ParallelOps.allReduce(count);
             missingCount = ParallelOps.allReduce(missingCount);
+            sum = ParallelOps.allReduce(sum);
             countWrong = ParallelOps.allReduce(countWrong);
+            double mean = sum/count;
+
+            for (int i = 0; i < filesPerProc; i++) {
+                int fileIndex = ParallelOps.worldProcRank * filesPerProc + i;
+                String fileId = (fileIndex < 100) ? "0" : "";
+                fileId += (fileIndex < 10) ? "0" + fileIndex : "" + fileIndex;
+                String filePath = fileDir + filePrefirx + fileId;
+                BufferedReader bf = new BufferedReader(new FileReader(filePath));
+                String line = null;
+                String splits[];
+                while ((line = bf.readLine()) != null) {
+                    splits = line.split("\\s+");
+                    double value = Double.valueOf(splits[2]);
+                    sdsum += (value - mean)*(value - mean);
+                }
 
 
+            }
+
+
+            sdsum = ParallelOps.allReduce(sdsum);
+            double sd = Math.sqrt(sdsum/count-1);
             if (ParallelOps.worldProcRank == 0) {
-                System.out.printf("Max : %.5f\nMin : %.5f\nTotalLinks : %.2f\nMissingCount : %.2f\nCountWrong : %d",
-                        max, min, count, missingCount, countWrong);
+                System.out.printf("Max : %.5f\nMin : %.5f\nTotalLinks : %.2f\nMissingCount : %.2f\nCountWrong : %d" +
+                                "\nMean : %.5f\n Over1k %d\nSD %.5f",
+                        max, min, count, missingCount, countWrong, mean, countOver1k, sd);
             }
         } catch (MPIException e) {
             e.printStackTrace();
