34d72f82dc1f71e7977aa56f96c5b627e648741f
pulasthi <pulasthi911@gmail.com>
2019-06-19 23:31:53 -0400

    update

--- src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergHeatMap.java
+++ src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergHeatMap.java
@@ -2,10 +2,7 @@ package edu.indiana.soic.spidal.binary.tools.mpitools;
 
 import mpi.MPIException;
 
-import java.io.BufferedReader;
-import java.io.DataOutput;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class BloombergHeatMap {
     public static void main(String[] args) {
@@ -15,11 +12,14 @@ public class BloombergHeatMap {
             int para = ParallelOps.worldProcsCount;
             int numPoints = Integer.valueOf(args[0]);
             String mdspoints = args[1];
+            String fileDir = args[2];
+            String outFile = args[3];
             double localMin = Double.MAX_VALUE;
             double localMax = Double.MIN_VALUE;
             double[][] points = new double[numPoints][3];
             int pointsPerProc = numPoints/para;
 
+            double heatmap[] = new double[1000*1000];
             try {
                 //read points
                 BufferedReader bf = new BufferedReader(new FileReader(mdspoints));
@@ -50,22 +50,66 @@ public class BloombergHeatMap {
             }
 
             for (int i = start; i < end; i++) {
-                for (double[] point : points) {
-                    double dist = euclideanDist(points[i], point);
-                    localMin = (dist < localMin) ? dist : localMin;
-                    localMax = (dist > localMax) ? dist : localMax;
-                }
+//                for (double[] point : points) {
+//                    double dist = euclideanDist(points[i], point);
+//                    localMin = (dist < localMin) ? dist : localMin;
+//                    localMax = (dist > localMax) ? dist : localMax;
+//                }
                 if(i%100 == 0){
                     Utils.printMessage(".");
                 }
             }
 
+            Utils.printMessage("Done calculations");
             double max = ParallelOps.allReduceMax(localMax);
+            System.out.println("Max" + max);
             double min = ParallelOps.allReduceMin(localMin);
             System.out.println("Min" + min);
-            System.out.println("Max" + max);
 
-        }catch (MPIException e){
+
+//            int totalSplits = 192;
+//            String filePrefirx = "part_";
+//            int filesPerProc = totalSplits / ParallelOps.worldProcsCount;
+//
+//            for (int i = 0; i < filesPerProc; i++) {
+//                int fileIndex = ParallelOps.worldProcRank * filesPerProc + i;
+//                String fileId = (fileIndex < 100) ? "0" : "";
+//                fileId += (fileIndex < 10) ? "0" + fileIndex : "" + fileIndex;
+//                String filePath = fileDir + filePrefirx + fileId;
+//                BufferedReader bf = new BufferedReader(new FileReader(filePath));
+//                String line = null;
+//                String splits[];
+//                while ((line = bf.readLine()) != null) {
+//                    splits = line.split("\\s+");
+//
+//                    int row = Integer.valueOf(splits[0]);
+//                    int col = Integer.valueOf(splits[1]);
+//                    double valueOri = Double.valueOf(splits[2]);
+//                    double temp = euclideanDist(points[row], points[col]);
+//                    double valueMDS = (temp - min) / (max - min);
+//
+//                    //convert to indexes
+//                    int mdsindex_i = (int)Math.floor(valueMDS*1000);
+//                    int orindex_j = (int)Math.floor(valueOri*1000);
+//
+//                    heatmap[mdsindex_i*1000 + orindex_j] += 1;
+//                }
+//            }
+//
+//            PrintWriter outWriter = new PrintWriter(new FileWriter(outFile));
+//
+//            for (int i = 0; i < 1000; i++) {
+//                for (int j = 0; j < 1000; j++) {
+//                    double v = heatmap[i*1000 + j];
+//                    outWriter.print(v + ",");
+//                }
+//                outWriter.print("\n");
+//            }
+//
+//            outWriter.flush();
+//            outWriter.close();
+
+        }catch (MPIException e ){
             e.printStackTrace();
         }
     }
