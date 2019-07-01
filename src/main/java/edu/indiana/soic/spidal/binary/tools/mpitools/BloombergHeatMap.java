8f6eb4699f45828149c66fa6374432d28c8335cd
pulasthi <pulasthi911@gmail.com>
2019-07-01 13:32:37 -0400

    adding all reduce

--- src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergHeatMap.java
+++ src/main/java/edu/indiana/soic/spidal/binary/tools/mpitools/BloombergHeatMap.java
@@ -1,5 +1,6 @@
 package edu.indiana.soic.spidal.binary.tools.mpitools;
 
+import mpi.MPI;
 import mpi.MPIException;
 
 import java.io.*;
@@ -15,7 +16,7 @@ public class BloombergHeatMap {
             int numPoints = Integer.valueOf(args[0]);
             String mdspoints = args[1];
             String fileDir = args[2];
-            String outFile = args[3];
+            String outFileDir = args[3];
             String indexFile = args[4];
             double localMin = Double.MAX_VALUE;
             double localMax = Double.MIN_VALUE;
@@ -23,8 +24,13 @@ public class BloombergHeatMap {
             Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
             int pointsPerProc = numPoints/para;
             ParallelOps.worldProcsComm.barrier();
-
+            double max = 2.5511707999080118;
+            double min = 0.0;
+            double minOri = 0.61600;
+            double maxOri = 14950.00;
             double heatmap[] = new double[1000*1000];
+            double histtroOri[] = new double[1000];
+            double histtroMDS[] = new double[1000];
             try {
                 //read points
                 BufferedReader bf = new BufferedReader(new FileReader(mdspoints));
@@ -67,17 +73,15 @@ public class BloombergHeatMap {
 //            int count = 0;
 //            for (int i = start; i < end; i++) {
 //                count++;
-////                for (double[] point : points) {
-////                    double dist = euclideanDist(points[i], point);
-////                    localMin = (dist < localMin) ? dist : localMin;
-////                    localMax = (dist > localMax) ? dist : localMax;
-////                }
+//                for (double[] point : points) {
+//                    double dist = euclideanDist(points[i], point);
+//                    double valueMDS = (dist - min) / (max - min);
+//                    int hisIn = (int)Math.floor(valueMDS*1000);
+//                    histtroMDS[hisIn]++;
+//                }
 //            }
 
-            double max = 2.5511707999080118;
-            double min = 0.0;
-            double minOri = 0.61600;
-            double maxOri = 14950.00;
+
 //            ParallelOps.worldProcsComm.barrier();
 //            Utils.printMessage("Done calculations");
 //            localMax = ParallelOps.allReduceMax(localMax);
@@ -117,12 +121,27 @@ public class BloombergHeatMap {
                     //convert to indexes
                     int mdsindex_i = (int)Math.floor(valueMDS*1000);
                     int orindex_j = (int)Math.floor(valueOri*1000);
-
+                    histtroOri[orindex_j]++;
+                    histtroMDS[mdsindex_i]++;
                     heatmap[mdsindex_i*1000 + orindex_j] += 1;
                 }
             }
 
-            PrintWriter outWriter = new PrintWriter(new FileWriter(outFile));
+            ParallelOps.worldProcsComm.barrier();
+            ParallelOps.allReduce(histtroMDS, MPI.SUM, ParallelOps.worldProcsComm);
+            ParallelOps.allReduce(histtroOri, MPI.SUM, ParallelOps.worldProcsComm);
+            ParallelOps.allReduce(heatmap, MPI.SUM, ParallelOps.worldProcsComm);
+            PrintWriter outWriter = new PrintWriter(new FileWriter(outFileDir + "/" + "heatmap.txt"));
+            PrintWriter outWriterhistMds = new PrintWriter(new FileWriter(outFileDir + "/" + "histoMDS.txt"));
+            PrintWriter outWriterhistOir = new PrintWriter(new FileWriter(outFileDir + "/" + "histoOri.txt"));
+
+            for (double val : histtroMDS) {
+                outWriterhistMds.print(val+",");
+            }
+
+            for (double val : histtroOri) {
+                outWriterhistOir.print(val+",");
+            }
 
             for (int i = 0; i < 1000; i++) {
                 for (int j = 0; j < 1000; j++) {
@@ -134,6 +153,10 @@ public class BloombergHeatMap {
 
             outWriter.flush();
             outWriter.close();
+            outWriterhistMds.flush();
+            outWriterhistMds.close();
+            outWriterhistOir.flush();
+            outWriterhistOir.close();
             ParallelOps.tearDownParallelism();
 
         }catch (MPIException e ){
